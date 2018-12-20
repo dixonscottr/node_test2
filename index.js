f83bb2d2f0c1987a6650cc51a2f8df8f5bdd49fe
Scott Dixon <dixon.scottr@gmail.com>
2018-12-20 10:22:58 -0500

    Update main JS file

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 const tracer = require('dd-trace').init({
-  debug: true
+    debug: true
 })
-
-const express = require('express')
-const request = require('request')
-const proxy = require('express-http-proxy');
+const express = require('express');
+const http = require('http');
+const request = require('request');
 
 tracer.use('express')
 
+const app = express();
 
-const app = express()
+app.get('/test', function (req, res) {
+    console.log("sending request...")
 
-app.use('/proxy', proxy('http://localhost:8888/'));
-
-app.get('/ids/:id', function(req, res){ 
-
-	var url = "http://127.0.0.1:8888/ids/"
-	url += req.params['id']
-
-    request(url, function (error, response, body) {
-
-          res.send(body)
-
-    });
-
+    request('http://localhost:8888/', function (error, response, body) {
+      res.send('')
+      if (error) {
+        console.log(error)
+      }
+      console.log("headers: ", response.headers);
+      console.log("body: ", body)
+      });
 })
 
-app.listen(3000)
+app.get('/test2', function (req, res) {
+    console.log("recieving request...")
+    console.log("headers: ", JSON.stringify(req.headers))
+    res.end()
+})
+
+
+
+const server = app.listen(8081, function () {
+
+    const host = server.address().address
+    const port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
